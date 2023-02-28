b8cd6770e3a6204bc2deead88a9a8b307015c2a2
Chris Coffee <chrico@uab.edu>
2023-02-28 09:49:52 -0600

    combined old and new (convert_leipzig_data.sh)

--- convert_leipzig_data.sh
+++ convert_leipzig_data.sh
@@ -1,7 +1,14 @@
-#cat data/raw/en*.txt > data/combined.txt
-find "$1" -name \*.txt -exec sh -c 'cat {} >> data/tmp.txt' \;
-#cut -f 2 data/combined.txt > data/tmp.txt
+# add standard data
+cat data/raw/en*.txt > data/combined.txt
+cut -f 2 data/combined.txt > data/tmp.txt
+
+# add extra data
+find "$1" -name "$2" -exec sh -c 'cat $1 > data/tmp2.txt' {} \;
+uniq data/tmp2.txt >> data/tmp.txt
+
+# do remaining steps
 shuf data/tmp.txt > data/data.txt
+rm data/tmp2.txt
 rm data/tmp.txt
-#rm data/combined.txt
+rm data/combined.txt
 wc -l data/data.txt
